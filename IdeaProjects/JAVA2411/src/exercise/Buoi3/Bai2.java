ddf33c4e56be519f158a360d4422216b7b407549
Kv-teck <vukhiet2k3@gmail.com>
2024-11-28 14:29:24 +0700

    add

--- IdeaProjects/JAVA2411/src/exercise/Buoi3/Bai2.java
+++ IdeaProjects/JAVA2411/src/exercise/Buoi3/Bai2.java
@@ -1,5 +1,7 @@
 package exercise.Buoi3;
 
+import java.util.Scanner;
+
 public class Bai2 {
     //    Đổi sang số nhị phân của 1 số nguyên dương n
 //    Ví dụ nhập sao số dương 8 in ra màn hình số nhị phân của nó là 1000
@@ -7,6 +9,16 @@ public class Bai2 {
 //            9 -> 1001
 //            16 -> 10000
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap n");
+        int n = scanner.nextInt();
 
+        String soNhiPhan = "";
+        while (n > 0) {
+            soNhiPhan += (n % 2);
+            n = n / 2;
+        }
+        System.out.println(soNhiPhan);
+        scanner.close();
     }
 }
